1f2d3d8e698444a72f94ba214db870ebacdb08b0
cxbdasheng <cxbdasheng@gmail.com>
2025-08-18 13:25:30 +0800

    chore: 更新百度统计代码- 替换分析.js中的百度统计脚本链接 - 新的脚本链接为 https://hm.baidu.com/hm.js?c327cf2e762ca5a877b44bc170375ed6

--- docs/javascripts/analysis.js
+++ docs/javascripts/analysis.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?075ac51c9b21120b5033bdb664b62d34";
+  hm.src = "https://hm.baidu.com/hm.js?c327cf2e762ca5a877b44bc170375ed6";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
